1cfe5ec41ede5bbd11381f2a2c087fb6b7fd20ad
kehman18 <kehindeadekola96@gmail.com>
2023-02-27 11:08:32 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: the string to be counted
+* Return: always 0
+*/
+
+int _strlen(char *s)
+{
+	int len_str = 0;
+
+	while (*s != '0')
+	{
+		len_str++;
+		s++;
+	}
+
+	return (len_str);
+}
