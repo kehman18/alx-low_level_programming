ee893e755a5a7d46452e6664e954a16e04f42a26
kehman18 <kehindeadekola96@gmail.com>
2023-02-27 10:01:49 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+* reset_to_98 -  takes a pointer to an int as parameter
+* @n: input given to be used
+* Return: since void, it should return a character if at all
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
